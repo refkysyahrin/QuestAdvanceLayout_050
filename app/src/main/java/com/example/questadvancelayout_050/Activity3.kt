ec6d569f4f03360a2390a57dc968377c1953007c
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-10-24 18:57:00 +0700

    membuat column

--- app/src/main/java/com/example/questadvancelayout_050/Activity3.kt
+++ app/src/main/java/com/example/questadvancelayout_050/Activity3.kt
@@ -1,8 +1,33 @@
 package com.example.questadvancelayout_050
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun ProfileScreen() {
+fun Profil(modifier: Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
+
+    }
 }
