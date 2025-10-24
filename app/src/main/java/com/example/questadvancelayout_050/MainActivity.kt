bdc9c12b35d5d04878b19bc7393aa9b2b3907492
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-10-24 15:21:34 +0700

    Mengganti nama fungsi

--- app/src/main/java/com/example/questadvancelayout_050/MainActivity.kt
+++ app/src/main/java/com/example/questadvancelayout_050/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAdvanceLayout_050Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ActivitasPertama(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
