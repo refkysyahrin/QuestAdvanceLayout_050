7bfaeaf1d4a67d633e5cc10a95104c4be60d1084
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-10-24 14:59:20 +0700

    Menambahkan ActivityPertama memberikan padding dan fillMaxSize

---
+++ app/src/main/java/com/example/questadvancelayout_050/UIPertama.kt
@@ -0,0 +1,17 @@
+package com.example.questadvancelayout_050
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import java.lang.reflect.Modifier
+
+@Composable
+fun ActivitasPertama(modifier: Modifier){
+    Column(modifier= Modifier
+        .padding(top=100.dp)
+        .fillMaxSize(),
+        horizontalAlignment = alignment.CenterHorizontal
+    )
+}
