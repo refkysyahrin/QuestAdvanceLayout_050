986375901d17a46d17b89801fd187fa557eafa87
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-10-24 20:37:54 +0700

    Menambahkan informasi dari user yaitu nama user

--- app/src/main/java/com/example/questadvancelayout_050/Activity3.kt
+++ app/src/main/java/com/example/questadvancelayout_050/Activity3.kt
@@ -11,14 +11,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Profil(modifier: Modifier) {
@@ -74,9 +80,16 @@ fun Profil(modifier: Modifier) {
                     .size(40.dp)
                     .clip(CircleShape)
             )
-
-
-
         }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Informasi User
+       Text(
+           text = stringResource(id = R.string.nama),
+           fontSize = 30.sp,
+           fontFamily = FontFamily.SansSerif,
+           color = Color.White,
+           modifier = Modifier.padding(top = 15.dp)
+       )
     }
 }
