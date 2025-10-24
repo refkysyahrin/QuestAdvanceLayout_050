1f21d77ea3e310c3ab4a512ff6ef2f3e2a3b8d92
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-10-24 15:30:20 +0700

    Menambahkan card mengubah ukuran, jarak dan memberikan warna.

--- app/src/main/java/com/example/questadvancelayout_050/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_050/UIPertama.kt
@@ -3,22 +3,28 @@ package com.example.questadvancelayout_050
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import java.lang.reflect.Modifier
+
 
 @Composable
 fun ActivitasPertama(modifier: Modifier){
     Column(modifier= Modifier
         .padding(top=100.dp)
         .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontal
+        horizontalAlignment = Alignment.CenterHorizontally
     ){
         Text(
             text = stringResource(id= R.string.prodi),
@@ -33,6 +39,15 @@ fun ActivitasPertama(modifier: Modifier){
 
         Spacer(modifier = Modifier.height(height = 25.dp))
 
-        )
+        Card(
+            modifier = Modifier
+                .height(height = 160.dp)
+                .fillMaxWidth(fraction = 1f)
+                .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )
+        ) {}
+
     }
 }
