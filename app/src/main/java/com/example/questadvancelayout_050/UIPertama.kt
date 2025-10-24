c4c69af05633eeeb3a138ad87d6f9c049a4342df
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-10-24 15:07:00 +0700

    Menambahkan text untuk univ

--- app/src/main/java/com/example/questadvancelayout_050/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_050/UIPertama.kt
@@ -3,10 +3,12 @@ package com.example.questadvancelayout_050
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import java.lang.reflect.Modifier
 
 @Composable
@@ -17,10 +19,16 @@ fun ActivitasPertama(modifier: Modifier){
         horizontalAlignment = Alignment.CenterHorizontal
     ){
         Text(
-            text = stringResource((id = R.string.prodi),
+            text = stringResource(id= R.string.prodi),
                 fontSize = 30.sp,
                 fontWeight = FontWeight.Bold
             )
+
+        Text(
+            text = stringResource(id = R.string.univ),
+            fontSize = 18.sp
+        )
+
         )
     }
 }
