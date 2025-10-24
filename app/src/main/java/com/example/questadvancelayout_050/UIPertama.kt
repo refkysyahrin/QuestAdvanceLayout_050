1c19559f414f4a6eb0699bb5d7b93b2d285ea065
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-10-24 15:55:49 +0700

    Menambahkan text copy di tengah bawah

--- app/src/main/java/com/example/questadvancelayout_050/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_050/UIPertama.kt
@@ -90,5 +90,12 @@ fun ActivitasPertama(modifier: Modifier){
     Box(
         modifier = Modifier
             .fillMaxSize()
-    )
+    ){
+        Text(
+            text = stringResource(id= R.string.copy),
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 50.dp)
+        )
+    }
 }
