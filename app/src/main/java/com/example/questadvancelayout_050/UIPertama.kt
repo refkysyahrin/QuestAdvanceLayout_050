767ebfb50c1aa124944b88a225e6fecf06a60ffa
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-10-24 15:41:54 +0700

    Menambahkan text nama dalam wadah

--- app/src/main/java/com/example/questadvancelayout_050/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_050/UIPertama.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -67,6 +68,15 @@ fun ActivitasPertama(modifier: Modifier){
                 )
 
                 Spacer(modifier = Modifier.width(width = 30.dp))
+                Column() {
+                    Text(
+                        text = stringResource(id = R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
+                }
 
 
             }
